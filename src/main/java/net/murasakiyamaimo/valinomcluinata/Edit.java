950e15fc2937bec32b060e58598f3989f4b36652
BuildTools <unconfigured@null.spigotmc.org>
2025-03-31 15:48:57 +0900

    for public synthesizer

--- src/main/java/net/murasakiyamaimo/valinomcluinata/Edit.java
+++ src/main/java/net/murasakiyamaimo/valinomcluinata/Edit.java
@@ -11,10 +11,8 @@ public class Edit extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Edit.class.getResource("Editor" + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Edit.class.getResource("Synthesizer" + ".fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-        EditorController controller = fxmlLoader.getController();
-        Synthesizer syn = new Synthesizer();
         double[] frequency = {261.63, 392.44, 327.03, 490.55};
         stage.setTitle("ValinomCluinata");
         stage.setScene(scene);
